1ac2d69f966fc92e206d6f63ed8caef84de702bd
Gustavo Souza Sales <20242ireads0009@ifba.edu.br>
2025-11-08 16:04:40 -0300

    ADD: Interface Gráfica da atividade 11

---
+++ br/com/ifba/atividade11/prg03persistence/src/main/java/br/com/ifba/CursoSave.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.ifba;
+import br.com.ifba.curso.view.CursoAdicionar;
+import br.com.ifba.curso.view.CursoEditar;
+import br.com.ifba.curso.view.CursoListar;
+
+/**
+ *
+ * @author gugsa
+ */
+public class CursoSave {
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+        new CursoListar().setVisible(true);
+    });
+    }
+}
